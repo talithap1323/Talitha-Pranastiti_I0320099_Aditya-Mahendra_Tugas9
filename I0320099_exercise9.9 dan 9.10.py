3b8522e9577c67dae2501a6645cd32308cd87967
talithap1323 <80144802+talithap1323@users.noreply.github.com>
2021-04-30 23:41:34 +0700

    Add files via upload

---
+++ I0320099_exercise9.9 dan 9.10.py
@@ -0,0 +1,19 @@
+import array
+
+# EXERCISE 9.9
+A = array.array('i', [100, 200, 300, 400, 500])
+print(A)
+
+A[1] = -700 #mengubah elemen kedua
+A[4] = 800  #mengubah elemen kelima
+print(A)
+
+# EXERCISE 9.10
+# nilai awal sebelum dibalik
+print(A)
+
+# membalik urutan elemen array
+A.reverse()
+
+# nilai akhir setelah dibalik
+print(A)
